b6921bebd69ce249579dbdf43a28f8c1ce4d8e74
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-06-14 10:48:01 +0530

    Time: 250 ms (15.35%), Space: 121 MB (6.3%) - LeetHub

---
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        queue<int>q;
+        unordered_map<int,int>mp;
+        for(int i=0; i<n-1; i++){
+            if(nums[i] == nums[i+1]) q.push(nums[i]);
+            mp[nums[i]]++;
+        }
+        mp[nums[n-1]]++;
+
+        int val = nums[0]+1;
+        int incr = 0;
+        while(!q.empty()){
+            while(mp[val] > 0 || q.front() >= val){
+                val++;
+            }
+            int num = q.front();
+            q.pop();
+            incr += (val - num);
+            val++;
+            // cout<<incr<<" ";
+        }
+        // 1 1 2 2 3 7
+       
+
+
+
+
+
+        
+
+        return incr;
+    }
+};
