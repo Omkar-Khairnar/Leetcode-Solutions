bb388e82ffbdb1dcd49a6560cc7bad76853d4a6c
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-07-08 15:08:02 +0530

    Time: 600 ms (50%), Space: 493.8 MB (25%) - LeetHub

--- 3482-construct-string-with-minimum-cost/3482-construct-string-with-minimum-cost.cpp
+++ 3482-construct-string-with-minimum-cost/3482-construct-string-with-minimum-cost.cpp
@@ -2,61 +2,92 @@ class TrieNode
 {
     public:
     TrieNode *links[26];
-    bool isWordCompleted = false; 
-    int cost = INT_MAX;
+    bool isWordCompleted; 
+    int cost;
 
     TrieNode(){
-
+        isWordCompleted = false; //Initially setting word completed as False (will be true is node contains last char of string);
         for(int i=0; i<26; i++){
-            links[i]=NULL;
+            this->links[i]=NULL;
         }
+        cost = INT_MAX;
     }
 };
 
+class Trie{
+    public:
+    TrieNode *root;
+
+    //Constructor
+    Trie(){
+        root = new TrieNode();
+    }
+
+    //Return true OR false isWord present in Trie or not.
+    int searchWord(string &s){
+        TrieNode *curr = root;
+        for(int i=0; i<s.size(); i++){
+            if(curr->links[s[i]-'a'] == NULL){
+                return INT_MIN;
+            }
+            curr = curr->links[s[i]-'a'];
+        }
+        if(curr->isWordCompleted){
+            return curr->cost;
+        }
+        return INT_MAX;
+    }
+
+    //Inserts Word in Trie
+    void insertWord(string &s, int cost){
+        TrieNode *curr = root;
+        for(int i=0; i<s.size(); i++){
+            if(curr->links[s[i]-'a'] == NULL){
+                curr->links[s[i]-'a'] = new TrieNode();
+            }
+            curr = curr->links[s[i]-'a'];
+        }
+        curr->isWordCompleted=true;
+        curr->cost = min(curr->cost, cost);
+        
+    }
+
+};
 
 class Solution {
 public:
-    long long fun(int idx, string &target, TrieNode *root, vector<int>&dp){
+    long long fun(int idx, string &target, Trie &trie, vector<int>&dp){
         if(idx == target.size()){
             return 0;
         }
+        
         if(dp[idx] != -1) return dp[idx];
         long long ans = INT_MAX;
-        TrieNode *curr = root;
+        TrieNode *curr = trie.root;
         for(int i = idx; i<target.size(); i++){
             char ch = target[i];
+            if(curr->links[ch-'a'] == NULL) break;
             curr = curr->links[target[i]-'a'];
-            if(curr){
-                if(curr->isWordCompleted){
-                    int val = curr->cost;
-                    long long nextItr =  fun(i+1, target, root, dp);
-                    ans = min(ans, val + nextItr);
+            int val = curr->cost;
+            if(val != INT_MAX){
+                if(val != INT_MAX){
+                    ans = min(ans, val + fun(i+1, target, trie, dp));
                 }
-            }else break;
+            }
         }
 
         return dp[idx] = ans;
     }
     int minimumCost(string target, vector<string>& words, vector<int>& costs) {
-        TrieNode *root = new TrieNode();
+        Trie trie;
         int n = target.size();
         vector<int>dp(n+1,-1);
         for(int i=0; i<words.size(); i++){
-            string s = words[i];
-            TrieNode *curr = root;
-            for(int j=0; j<s.size(); j++){
-                if(curr->links[s[j]-'a'] == NULL){
-                    curr->links[s[j]-'a'] = new TrieNode();
-                }
-                curr = curr->links[s[j]-'a'];
-            }
-            curr->isWordCompleted=true;
-            curr->cost = min(curr->cost, costs[i]);
+            trie.insertWord(words[i], costs[i]);
         }
 
-        long long val = fun(0, target, root, dp);
+        long long val = fun(0, target, trie, dp);
         if(val >= INT_MAX) return -1;
         return val; 
     }
 };
-
