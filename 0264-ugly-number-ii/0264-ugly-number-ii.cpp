ce6cb5a1041c94334e59d9278684ea1b6f29466b
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-08-18 15:10:52 +0530

    Time: 5 ms (57.79%), Space: 7.8 MB (99.21%) - LeetHub

---
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        int num[n + 1];
+        num[0] = 1;
+        int t2=0, t3=0, t5=0;
+        for(int i=1; i<n; i++){
+            num[i] = min(num[t2]*2, min(num[t3]*3, num[t5]*5));
+            if(num[i] == num[t2]*2) t2++;
+            if(num[i] == num[t3]*3) t3++;
+            if(num[i] == num[t5]*5) t5++;
+        }
+
+        return num[n-1];
+    }
+};
