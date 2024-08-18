fd38f60b6cf1a9713209dbdf3d7481b62e65840a
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-08-18 12:36:16 +0530

    Time: 181 ms (72.41%), Space: 87.8 MB (83.82%) - LeetHub

--- 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.cpp
+++ 2067-maximum-number-of-points-with-cost/2067-maximum-number-of-points-with-cost.cpp
@@ -1,52 +1,32 @@
 class Solution {
 public:
-    // long long fun(int i, int j, vector<vector<int>>& points, vector<vector<long long>>& dp){
-    //     if(i == 0) return points[i][j];
-
-    //     if(dp[i][j] != -1) return dp[i][j];
-    //     int n = points[0].size();
-    //     long long maxi = 0;
-    //     for(int k=0; k<n; k++){
-    //         maxi = max(maxi, points[i][j] + fun(i-1, k, points, dp) - abs(j-k));
-    //     }
-
-    //     return dp[i][j] = maxi;
-    // }
-
     long long maxPoints(vector<vector<int>>& points) {
         int m = points.size(), n = points[0].size();
-        vector<vector<long long>>dp(m, vector<long long>(n,0));
+        vector<long long>prev(n,0);
+        vector<long long>curr(n,0);
         long long maxi = 0;
         for(int j=0; j<n;  j++){
-            dp[0][j] = points[0][j];
-            maxi = max(maxi, dp[0][j]);
+            prev[j] = points[0][j];
+            maxi = max(maxi, prev[j]);
         }
 
         for(int i=1; i<m; i++){
-            map<long long,int, greater<int>>mp;
+            long long mxVal = prev[0];
+
             for(int j=0; j<n; j++){
-                mp[dp[i-1][j]-j]++;
+                mxVal = max(mxVal, prev[j]+j);
+                curr[j] = mxVal + points[i][j] - j;
             }
-            for(int j=0; j<n; j++){
-                dp[i][j] = points[i][j] + j + mp.begin()->first;
-                maxi = max(maxi, dp[i][j]);
-                mp[dp[i-1][j] -j]--;
-                if(mp[dp[i-1][j] -j] == 0){
-                    mp.erase(dp[i-1][j] -j);
-                }
-            }
-            mp.clear();
-            for(int j=0; j<n; j++){
-                mp[dp[i-1][j]+j]++;
-            }
+            mxVal = prev[n-1]-(n-1);
             for(int j=n-1; j>=0; j--){
-                dp[i][j] = max(dp[i][j], points[i][j] - j + mp.begin()->first);
-                maxi = max(maxi, dp[i][j]);
-                mp[dp[i-1][j] + j]--;
-                if(mp[dp[i-1][j] + j] == 0){
-                    mp.erase(dp[i-1][j] + j);
-                }
+                mxVal = max(mxVal, prev[j]-j);
+                curr[j] = max(curr[j], mxVal + points[i][j] + j);
             }
+            prev = curr;
+        }
+
+        for(int j=0; j<n; j++){
+            maxi = max(maxi, curr[j]);
         }
 
 
