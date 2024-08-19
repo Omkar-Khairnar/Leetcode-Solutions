022685e6f8cfacb671e33d39757865a6d84af84e
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-08-19 12:24:52 +0530

    Time: 28 ms (16.71%), Space: 157.1 MB (10.43%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.cpp
@@ -1,22 +1,23 @@
 #define ll long long 
 class Solution {
 public:
-    ll fun(int curr, int copiedLast, int n){
+    ll fun(int curr, int copiedLast, int n, vector<vector<int>>&dp){
         if(curr == n) return 0;
 
+        if(dp[curr][copiedLast] != -1) return dp[curr][copiedLast];
         ll ans = INT_MAX;
         if(copiedLast > 0 && curr + copiedLast <= n){
-            ans = min(ans, 1+fun(curr+copiedLast, copiedLast, n));
+            ans = min(ans, 1+fun(curr+copiedLast, copiedLast, n, dp));
         }
         if(curr + curr <= n && curr != copiedLast){
-            ans = min(ans, 1+fun(curr, curr, n));
+            ans = min(ans, 1+fun(curr, curr, n, dp));
         }
 
-        return ans;
+        return dp[curr][copiedLast] = ans;
     }
     int minSteps(int n) {
-        
-        int ans = fun(1, 0, n);
+        vector<vector<int>>dp(n+1, vector<int>(n+1, -1));
+        int ans = fun(1, 0, n, dp);
         return ans;
     }
 };
