477f0fa0b87140edbb0115c652922197125d86aa
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-09-16 11:19:07 +0530

    Time: 206 ms (48.89%), Space: 171.5 MB (67.03%) - LeetHub

--- 3528-reach-end-of-array-with-max-score/3528-reach-end-of-array-with-max-score.cpp
+++ 3528-reach-end-of-array-with-max-score/3528-reach-end-of-array-with-max-score.cpp
@@ -3,26 +3,34 @@ class Solution {
 public:
     long long findMaximumScore(vector<int>& nums) {
         int n = nums.size();
-        vector<ll>dp(n,0);
-        stack<int>st;
-        st.push(0);
+//Using Next Greater Element -> Not Needed        
+        // vector<ll>dp(n,0);
+        // stack<int>st;
+        // st.push(0);
+        // for(int i=1; i<n; i++){
+        //     ll maxi = 0;
+        //     while(!st.empty() && nums[i] > nums[st.top()]){
+        //         maxi = max(maxi, ((ll)(i-st.top()) * nums[st.top()]) + dp[st.top()]);
+        //         st.pop();
+        //     }
+
+        //     dp[i] = maxi;
+        //     st.push(i);
+        // }
+
+        // while(!st.empty()){
+        //     dp[n-1] = max(dp[n-1], (ll)(n-1-st.top())*nums[st.top()] + dp[st.top()]);
+        //     st.pop();
+        // }
+        // return dp[n-1];
+
+        int curr = nums[0];
+        ll sum = 0;
         for(int i=1; i<n; i++){
-            ll maxi = 0;
-            while(!st.empty() && nums[i] > nums[st.top()]){
-                maxi = max(maxi, ((ll)(i-st.top()) * nums[st.top()]) + dp[st.top()]);
-                st.pop();
-            }
-
-            dp[i] = maxi;
-            st.push(i);
+            sum += curr;
+            if(nums[i] > curr) curr = nums[i];
         }
 
-        while(!st.empty()){
-            dp[n-1] = max(dp[n-1], (ll)(n-1-st.top())*nums[st.top()] + dp[st.top()]);
-            st.pop();
-        }
-
-
-        return dp[n-1];
+        return sum;
     }
 };
