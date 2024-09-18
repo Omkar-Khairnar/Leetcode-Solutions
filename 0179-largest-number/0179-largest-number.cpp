870e9ea502ca7ff581f6540001bfb5f3d46d8d35
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-09-18 11:11:35 +0530

    Time: 7 ms (61.31%), Space: 17.3 MB (36.02%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -9,7 +9,31 @@ public:
         }
 
         if(isAllZero) return "0";
-
+//This approach is passing all the cases except of scenerio [34323,3432]
+        //  sort(vec.begin(), vec.end(), [](const string &a, const string &b){
+            // int lenA = a.size(), lenB = b.size();
+            // int i=0, j=0;
+            // while(i < lenA && j < lenB){
+            //     if(a[i] == b[j]){
+            //         i++, j++;
+            //     }
+            //     else break;
+            // }
+            // if(i == lenA && j == lenB) return a > b;
+            // else if(i == lenA){
+            //     while(a[lenA-1] == b[j]){
+            //         j++;
+            //     }
+            //     return a[lenA-1] > b[j]; 
+            // }
+            // else if(j == lenB){
+            //     while(b[lenB-1] == a[i]){
+            //         i++;
+            //     }
+            //     return a[i] > b[lenB-1];
+            // }
+            // return a > b;
+        // });
         sort(vec.begin(), vec.end(), [](const string &a, const string &b){
             string AB = a+b;
             string BA = b+a;
