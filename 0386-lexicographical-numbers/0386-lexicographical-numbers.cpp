0973eb993db734a2f89e74dc1d3f793b750d2a8a
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-09-22 16:45:25 +0530

    Time: 8 ms (72.41%), Space: 12.5 MB (85.59%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -13,7 +13,27 @@ public:
     }
     vector<int> lexicalOrder(int n) {
         vector<int>ans;
-        fun(0, ans, n);
+        // fun(0, ans, n);
+        // return ans;
+
+        int curr = 1;
+        for(int i=1; i<=n; i++){
+            ans.push_back(curr);
+            if(curr*10 <= n){
+                curr *= 10;
+            }
+            else if(curr % 10 != 9 && curr+1 <= n){
+                curr++;
+            } 
+            else{
+                while((curr/10) % 10 == 9){
+                    curr /= 10;
+                }
+                curr = curr/10;
+                curr++;
+            }
+        }
+        
         return ans;
     }
 };
