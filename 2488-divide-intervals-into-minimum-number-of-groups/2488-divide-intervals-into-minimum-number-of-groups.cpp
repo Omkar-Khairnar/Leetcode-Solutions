cae859400d69eb35d89ae89393147bf5714b66b9
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-10-13 15:30:46 +0530

    Time: 439 ms (18.77%), Space: 134.1 MB (29.59%) - LeetHub

--- 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
+++ 2488-divide-intervals-into-minimum-number-of-groups/2488-divide-intervals-into-minimum-number-of-groups.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-    int minGroups(vector<vector<int>>& intervals) {
-        // Convert the intervals to two events
-        // start as {start, 1} and end as {end, -1}
-        vector<pair<int, int>> intervalsWithEnd;
-        for (vector<int> interval : intervals) {
-            intervalsWithEnd.push_back({interval[0], 1});
-            intervalsWithEnd.push_back({interval[1] + 1, -1});
+    int minGroups(vector<vector<int>>& I) {
+        map<int, int>mp;
+
+        for(vector<int>v : I){
+            mp[v[0]]++;
+            mp[v[1]+1]--;
         }
 
-        // Sort the events according to the number and then by the value (1/-1).
-        sort(intervalsWithEnd.begin(), intervalsWithEnd.end());
-
-        int concurrentIntervals = 0;
-        int maxConcurrentIntervals = 0;
-        for (auto p : intervalsWithEnd) {
-            // Keep track of currently active intersecting intervals.
-            concurrentIntervals += p.second;
-            // Update the maximum number of active intervals.
-            maxConcurrentIntervals =
-                max(maxConcurrentIntervals, concurrentIntervals);
+        int ct = 0;
+        int ans = 0;
+        for(auto it = mp.begin(); it != mp.end(); it++){
+            ct += it->second;
+            ans = max(ans, ct);
         }
-
-        return maxConcurrentIntervals;
+        return ans;
     }
 };
