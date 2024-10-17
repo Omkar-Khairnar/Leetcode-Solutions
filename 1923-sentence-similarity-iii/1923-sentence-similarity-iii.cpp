6085a3231d4c35ca2c1bc3e1e0f40edd1519ea4c
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-10-17 15:27:51 +0530

    Time: 0 ms (100%), Space: 8.6 MB (55.36%) - LeetHub

--- 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
+++ 1923-sentence-similarity-iii/1923-sentence-similarity-iii.cpp
@@ -1,31 +1,5 @@
 class Solution {
 public:
-    int getLastOccurrance(string bigStr, string subStr){
-        reverse(bigStr.begin(), bigStr.end());
-        reverse(subStr.begin(), subStr.end());
-        int n = bigStr.size(), m= subStr.size();
-        int i=0, j=0;
-
-        string temp1 = "";
-        string temp2 = "";
-
-        while(i<n && j < m){
-            temp1 = "";
-            temp2 = "";
-            while(i < n && bigStr[i] != ' '){
-                temp1 += bigStr[i];
-                i++;
-            }
-            while(j < m && subStr[j] != ' '){
-                temp2 += subStr[j];
-                j++;
-            }
-            i++, j++;
-            if(temp1 != temp2) return -1;
-        }
-       
-        return (j==m+1) ? 0 : -1;
-    }
     bool areSentencesSimilar(string s1, string s2) {
         string bigStr = "";
         string subStr = "";
@@ -37,10 +11,6 @@ public:
         else{
             bigStr = s2;
             subStr = s1;
-        }
-
-        if(getLastOccurrance(bigStr, subStr) == 0){
-            return true;
         }
        
         stringstream sBig(bigStr);
@@ -55,30 +25,27 @@ public:
         while(sSub >> word){
             subVec.push_back(word);
         }
-        int n = bigVec.size(), m = subVec.size(); 
+        int n = subVec.size(), m = bigVec.size(); 
 
-        int i=0, j=0;
-        int ct = 0;
-        while(j < m){
-            if(i >= n || j>= m) break;
-            if(bigVec[i] == subVec[j]){
-                i++, j++;
-            }
-            else{
-                ct++;
-                while(i<n && bigVec[i] != subVec[j]){
-                    i++;
-                }
-            }
-        }
-     
-        if(ct > 1) return false;
-        if(ct == 0) return true;
-        if(ct == 1){
-            return j == m && i==n;
-        }
-        
+        int l1 = 0, r1 = n-1;
+        int l2 = 0, r2 = m-1;
 
-        return true;
+        while(l1 <= r1){
+            while(l1 < n && subVec[l1] == bigVec[l2]){
+                l1++, l2++;
+            }
+            while(r1 >=0 && subVec[r1] == bigVec[r2]){
+                r1--, r2--;
+            }
+            if(l1 == n || r1 == -1 || (l1 > r1)) return true;
+            return false;
+
+        }
+
+        return false;
+
     }
 };
+
+
+
