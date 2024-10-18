f7eaf090ef3d663a1eb6972c4658e88eb92317a8
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-10-18 11:33:50 +0530

    Time: 3 ms (99.75%), Space: 10.1 MB (82.14%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -16,24 +16,14 @@ public:
 
     //     return dp[idx][curr] = take+notTake;
     // }
+
     int count = 0;
     void fun(int idx, int curr, vector<int>&nums){
-        // if(idx == nums.size()){
-        //     if(curr == maxi) return 1;
-        //     return 0;
-        // }
         if(curr == maxi) count++;
-
-        // if(dp[idx][curr] != -1){
-        //     return dp[idx][curr];
-        // }
-
      
         for(int i= idx; i<nums.size(); i++){
              fun(i+1, curr|nums[i], nums);
         }
-
-        // return dp[idx][curr] = take+notTake;
         
     }
 
@@ -43,7 +33,9 @@ public:
         for(int u : nums) maxi = maxi | u;
         int n = nums.size();
         // vector<vector<int>>dp(n, vector<int>(maxi+1, -1));
-         fun(0, 0, nums);
+        // int ans =  fun(0, 0, dp, nums);
+        
+        fun(0, 0, nums);
 
         return count;
     }
