8f39026463e8edf1ccbd589a0c359bfe47b5bdf2
Omkar Khairnar <110295103+Omkar-Khairnar@users.noreply.github.com>
2024-10-18 11:32:04 +0530

    Time: 124 ms (12.24%), Space: 309.1 MB (9.05%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.cpp
@@ -1,29 +1,50 @@
 class Solution {
 public:
     int maxi = 0;
-    int fun(int idx, int curr, vector<vector<int>>&dp, vector<int>&nums){
-        if(idx == nums.size()){
-            if(curr == maxi) return 1;
-            return 0;
+    // int fun(int idx, int curr, vector<vector<int>>&dp, vector<int>&nums){
+    //     if(idx == nums.size()){
+    //         if(curr == maxi) return 1;
+    //         return 0;
+    //     }
+
+    //     if(dp[idx][curr] != -1){
+    //         return dp[idx][curr];
+    //     }
+
+    //     int notTake = fun(idx+1, curr, dp, nums);
+    //     int take = fun(idx+1, curr|nums[idx], dp, nums);
+
+    //     return dp[idx][curr] = take+notTake;
+    // }
+    int count = 0;
+    void fun(int idx, int curr, vector<vector<int>>&dp, vector<int>&nums){
+        // if(idx == nums.size()){
+        //     if(curr == maxi) return 1;
+        //     return 0;
+        // }
+        if(curr == maxi) count++;
+
+        // if(dp[idx][curr] != -1){
+        //     return dp[idx][curr];
+        // }
+
+     
+        for(int i= idx; i<nums.size(); i++){
+             fun(i+1, curr|nums[i], dp, nums);
         }
 
-        if(dp[idx][curr] != -1){
-            return dp[idx][curr];
-        }
-
-        int notTake = fun(idx+1, curr, dp, nums);
-        int take = fun(idx+1, curr|nums[idx], dp, nums);
-
-        return dp[idx][curr] = take+notTake;
+        // return dp[idx][curr] = take+notTake;
+        
     }
+
 
 
     int countMaxOrSubsets(vector<int>& nums) {
         for(int u : nums) maxi = maxi | u;
         int n = nums.size();
         vector<vector<int>>dp(n, vector<int>(maxi+1, -1));
-        int ans = fun(0, 0, dp, nums);
+         fun(0, 0, dp, nums);
 
-        return ans;
+        return count;
     }
 };
